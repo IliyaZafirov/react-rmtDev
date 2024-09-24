353630526181f9917fcf18d981ed5e272fe2bd47
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-24 17:46:13 +0300

    showing the search results

--- src/components/JobList.tsx
+++ src/components/JobList.tsx
@@ -1,9 +1,13 @@
+import JobListItem from "./JobListItem";
+
 export function JobList({ jobItems }) {
-  return <ul className="job-list">
-    {
-      jobItems.map(item => item.title)
-    }
-  </ul>;
+  return (
+    <ul className="job-list">
+      {jobItems.map((item) => (
+        <JobListItem item={item} />
+      ))}
+    </ul>
+  );
 }
 
 export default JobList;
