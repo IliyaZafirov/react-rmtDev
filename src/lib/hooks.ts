21c9612081c3942d946f9b4f86b35e5583c7eba9
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-25 12:38:01 +0300

    slice jobItems (derived state)

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -4,6 +4,8 @@ export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const jobItemsSliced = jobItems.slice(0, 7);
+
   useEffect(() => {
     if (!searchText) return;
     const fetchData = async () => {
@@ -23,5 +25,5 @@ export function useJobItems(searchText: string) {
     fetchData();
   }, [searchText]);
 
-  return { jobItems, isLoading };
+  return { jobItemsSliced, isLoading };
 }
