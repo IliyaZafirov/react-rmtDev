9b81739e5fc7651db33b84ded4b30d2c8ba7d748
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-09-27 10:42:24 +0300

    render jobItem (custom type)

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,19 @@
 export type JobItem = {
-    id: number;
-    badgeLetters: string;
-    title: string;
-    company: string;
-    relevanceScore: number;
-    daysAgo: number;
-  };
-  
+  id: number;
+  badgeLetters: string;
+  title: string;
+  company: string;
+  relevanceScore: number;
+  daysAgo: number;
+};
+
+export type JobItemExpanded = JobItem & {
+  description: string;
+  qualifications: string[];
+  reviews: string[];
+  duration: string;
+  salary: string;
+  location: string;
+  coverImgURL: string;
+  companyURL: string;
+};
